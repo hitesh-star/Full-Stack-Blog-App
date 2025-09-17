5ef25de14f8b16f76f08009f6c53eac1a4604231
Hitesh <hitesh.parashar2016@gmail.com>
2025-09-17 19:53:17 +0530

    create Header Section

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${outfit.variable} antialiased`}
+        className={`${outfit.className}`}
       >
         {children}
       </body>
